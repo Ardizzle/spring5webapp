d997db3db1aaf7b98e3ccae68cfacc7ba8d8738a
ardel <ardel@192.168.1.247>
2018-11-04 21:48:18 -0600

    @manytomany Error.

---
+++ spring5webappExample/src/main/java/guru/springframework/spring5webapp/Spring5webappExampleApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappExampleApplication.class, args);
+	}
+}
